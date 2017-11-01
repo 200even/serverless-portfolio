594f0960e0b6c9d3f39aa161a5e4c58c6bc602be
Scott Ferguson <esfergus@gmail.com>
2017-11-01 09:31:17 -0500

    Deploy portfolio using Lambda and notify using SNS

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,30 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:973880726729:deployPortfolioTopic')
+    
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        portfolio_bucket = s3.Bucket('portfolio.fergusoniclabs.net')
+        build_bucket = s3.Bucket('portfoliobuild.fergusoniclabs.net')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildPortfolio.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio failed to deploy.")
+        raise
+    
+    return
